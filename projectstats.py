3f7f20945a7bbda026864645bafdbe8e933894a2
skye <skyewells675@gmail.com>
2025-05-09 00:49:30 -0500

    added another stat to project stats: content lines

--- projectstats.py
+++ projectstats.py
@@ -2,9 +2,13 @@ from glob import glob
 
 if __name__ == "__main__":
     # count lines of python
-    lineCount: int = 0
+    total_lineCount: int = 0
+    nonempty_lineCount: int = 0
     for file in glob("*.py"):
         with open(file, "r") as FILE:
-            lineCount += len(FILE.readlines())
+            lines: list[str] = FILE.readlines()
+            total_lineCount += len(lines)
+            nonempty_lineCount += len([line for line in lines if not line.strip() == ""])
 
-    print(f"lineCount: {lineCount}")
+    print(f"total lines   : {total_lineCount}\n" +
+          f"content lines : {nonempty_lineCount}")
